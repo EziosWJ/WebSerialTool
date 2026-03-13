a7c3379e3b8ea51a4bc6544bb7b2c4211e88330c
wangjian <656876071@qq.com>
2026-03-13 15:26:13 +0800

    获取依赖

---
+++ cmake/dependences.cmake
@@ -0,0 +1,54 @@
+# FetchContent缓存
+set(FETCHCONTENT_BASE_DIR ${CMAKE_SOURCE_DIR}/.deps)
+# libhv
+include(FetchContent)
+
+FetchContent_Declare(
+    libhv
+    GIT_REPOSITORY https://github.com/ithewei/libhv.git
+    GIT_TAG v1.3.4
+)
+
+FetchContent_MakeAvailable(libhv)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE hv)
+
+# nlohmann/json
+FetchContent_Declare(
+    json
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
+    GIT_TAG v3.11.3
+)
+
+FetchContent_MakeAvailable(json)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE nlohmann_json::nlohmann_json)
+
+# spdlog
+FetchContent_Declare(
+    spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    GIT_TAG v1.13.0
+)
+
+FetchContent_MakeAvailable(spdlog)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE spdlog::spdlog)
+
+# standone Asio
+FetchContent_Declare(
+    asio
+    GIT_REPOSITORY https://github.com/chriskohlhoff/asio.git
+    GIT_TAG asio-1-30-2
+)
+
+FetchContent_MakeAvailable(asio)
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+    ${asio_SOURCE_DIR}/asio/include
+)
+
+# 定义ASIO_STANDALONE宏，告诉编译器我们使用的是standalone Asio
+target_compile_definitions(${PROJECT_NAME} PRIVATE
+    ASIO_STANDALONE
+)
--- doc/personal/getLibs.md
+++ doc/personal/getLibs.md
@@ -4,6 +4,23 @@
 浅克隆：非常适合嵌入式 buildroot / CI
 
 ```shell
-git clone --depth 1 --branch v1.3.4 --single-branch https://github.com/ithewei/libhv.git
+git clone --depth 1 --branch v1.3.4 --single-branch https://github.com/ithewei/libhv.git libraries/sources/libhv
 ```
 特点：只下载 一个 commit
+
+
+#### 直观方便的管理方式
+
+`FetchContent`获取
+
+```shell
+include(FetchContent)
+
+FetchContent_Declare(
+    libhv
+    GIT_REPOSITORY https://github.com/ithewei/libhv.git
+    GIT_TAG v1.3.4
+)
+
+FetchContent_MakeAvailable(libhv)
+```
